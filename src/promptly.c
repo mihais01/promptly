6b0e01bc9ae7e866e44206af0a3ea85e5435cb27
Mihai <seleamihai@gmail.com>
2026-04-11 22:33:46 +0300

    Implement Library initialization function and configuration structure

--- src/promptly.c
+++ src/promptly.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include "promptly.h"
 
+static bool g_initialized = false;
+static struct promptly_config g_config = {
+    .write = NULL,
+    .read = NULL
+};
+
+void promptly_init(const struct promptly_config *config) {
+    if (config == NULL) {
+        return;
+    }
+
+    g_config = *config;
+    g_initialized = true;
+}
+
 void promptly_greet(void) {
+    if (!g_initialized) {
+        return;
+    }
+
     printf("Hello, Promptly\n");
 }
--- src/promptly.h
+++ src/promptly.h
@@ -1,6 +1,44 @@
 #ifndef PROMPTLY_H
 #define PROMPTLY_H
 
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+
+typedef uint32_t promptly_interface_t;
+
+struct promptly_config {
+
+    /* ===== I/O Operations ===== */
+
+    /*
+        * A function pointer to write a message. 
+        * The message is not guaranteed to be null-terminated
+        *   so the length parameter must be used.
+        * The interface parameter can be used to specify which interface to write to, 
+        *   for example, stdout or stderr.
+    */
+    void (*write)(promptly_interface_t interface, const char *message, size_t length);
+
+    /*
+        * A function pointer to read input. 
+        * The buffer is not guaranteed to be null-terminated
+        *   so the length parameter must be used.
+        * The interface parameter can be used to specify which interface to read from, 
+        *   for example, stdin.
+        * The function should return the number of bytes read, 
+        *   or a negative value on error.
+    */
+    ssize_t (*read)(promptly_interface_t interface, char *buffer, size_t length);
+};
+
+/* @brief Initializes the Promptly library with the provided configuration.
+ *
+ * @param[in] config A pointer to a promptly_config structure
+*/
+void promptly_init(const struct promptly_config *config);
+
 void promptly_greet(void);
 
 #endif
